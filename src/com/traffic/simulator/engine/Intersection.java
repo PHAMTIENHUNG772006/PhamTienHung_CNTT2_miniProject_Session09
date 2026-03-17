52ffddbcacb81e639cb4fc93e224ea43f0d84905
unknown <huyhoan13032006@gmail.com>
2026-03-17 16:49:01 +0700

    up

--- src/com/traffic/simulator/engine/Intersection.java
+++ src/com/traffic/simulator/engine/Intersection.java
@@ -1,4 +1,30 @@
 package com.traffic.simulator.engine;
 
+import com.traffic.simulator.entity.vehicle.PriorityVehicle;
+import com.traffic.simulator.entity.vehicle.Vehicle;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Semaphore;
+
 public class Intersection {
+    private final Semaphore semaphore;
+    private final LinkedBlockingQueue<Vehicle> waitingQueue;
+    private final int MAX_QUEUE_SIZE;
+
+    private TrafficLight trafficLight;
+
+    public Intersection(TrafficLight trafficLight, int MAX_QUEUE_SIZE, LinkedBlockingQueue<Vehicle> waitingQueue, Semaphore semaphore) {
+        this.trafficLight = trafficLight;
+        this.MAX_QUEUE_SIZE = MAX_QUEUE_SIZE;
+        this.waitingQueue = waitingQueue;
+        this.semaphore = semaphore;
+    }
+
+    public void enter(Vehicle vehicle) {
+
+
+
+    }
 }
--- src/com/traffic/simulator/exception/CollisionException.java
+++ src/com/traffic/simulator/exception/CollisionException.java
@@ -1,4 +1,7 @@
 package com.traffic.simulator.exception;
 
-public class CollisionException {
+public class CollisionException extends Exception {
+    public CollisionException(String message) {
+        super(message);
+    }
 }
